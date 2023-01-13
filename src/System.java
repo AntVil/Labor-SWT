3dbe0bfb814c7184eb6d351f6cf4bdd8bc39cad2
Anton Böhler <antboehler@gmail.com>
2023-01-13 15:03:01 +0100

    Update System.java

--- src/System.java
+++ src/System.java
@@ -3,12 +3,66 @@ public class System {
 	private float currentSensorVoltage;
 	private float currentValveStatus;
 	
-	public static void main(String[] args) {
-		
+	private static Alarm alarm;
+	private static Notification notification;
+	private static Log log;
+	
+	public enum PressureState {
+		CRITICALLY_LOW, LOW, OPTIMAL, HIGH, CRITICALLY_HIGH, DANGEROUSLY_HIGH
+	}
+	private PressureState pressureState = PressureState.OPTIMAL;
+	
+	public System() {
+		alarm = new Alarm();
+		notification = new Notification(); 
+		log = new Log();
 	}
 	
 	public void evaluatePressure(float pressure) {
+		PressureState nextPressureState;
+		if(pressure < 50.0f) {
+			alarm.playSignal();
+			notification.notify("operations-manager@kawum.de", "critically low N2 pressure");
+			nextPressureState = PressureState.CRITICALLY_LOW;
+		}else if(pressure <= 180) {
+			notification.notify("operations-manager@kawum.de", "low N2 pressure, refill needed");
+			notification.notify("maintenance-team@kawum.de", "low N2 pressure, refill needed");
+			nextPressureState = PressureState.LOW;
+		}else if(pressure < 220) {
+			nextPressureState = PressureState.OPTIMAL;
+		}else if(pressure <= 300) {
+			notification.notify("operations-manager@kawum.de", "critically high N2 pressure");
+			notification.notify("maintenance-team@kawum.de", "critically high N2 pressure");
+			nextPressureState = PressureState.HIGH;
+		}else {
+			alarm.playSignal();
+			notification.notify("operations-manager@kawum.de", "dangerously high N2 pressure");
+			notification.notify("maintenance-team@kawum.de", "dangerously high N2 pressure");
+			nextPressureState = PressureState.CRITICALLY_HIGH;
+			
+			if(pressure > 500) {
+				alarm.playAlarm();
+				nextPressureState = PressureState.DANGEROUSLY_HIGH;
+			}
+		}
 		
+		if(pressureState != nextPressureState) {
+			pressureState = nextPressureState;
+
+			if(pressureState == PressureState.CRITICALLY_LOW) {
+				log.writeLog("critically low pressure of " + pressure);
+			}else if(pressureState == PressureState.LOW) {
+				log.writeLog("low pressure of " + pressure);
+			}else if(pressureState == PressureState.OPTIMAL) {
+				log.writeLog("optimal pressure of " + pressure);
+			}else if(pressureState == PressureState.HIGH) {
+				log.writeLog("high pressure of " + pressure);
+			}else if(pressureState == PressureState.CRITICALLY_HIGH) {
+				log.writeLog("critically high pressure of " + pressure);
+			}else if(pressureState == PressureState.DANGEROUSLY_HIGH) {
+				log.writeLog("dangerously high pressure of " + pressure);
+			}
+		}
 	}
 	
 	public float getCurrentPressure() {
@@ -21,5 +75,17 @@ public class System {
 	
 	public float getCurrentValveStatus() {
 		return currentValveStatus;
+	}
+	
+	public Alarm getAlarm() {
+		return alarm;
+	}
+	
+	public Notification getNotification() {
+		return notification;
+	}
+	
+	public Log getLog() {
+		return log;
 	}
 }

54c6425066abdc94acfaf2165c2db392b0bbc3fe
RafTob <83407665+RafTob@users.noreply.github.com>
2023-01-13 14:58:27 +0100

    Notification Test

--- src/NotificationTest.java
+++ src/NotificationTest.java
@@ -1,4 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class NotificationTest {
+
+    @Test
+    void sendLog(){
+        Notification notification = new Notification;
+        assertEquals("Works",notification.notify("test@test.com", "This is a test"));
+    }
 
 }

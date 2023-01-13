865e85bb1633a514d4750de8187a98915d0c5d8c
RafTob <83407665+RafTob@users.noreply.github.com>
2023-01-13 14:35:07 +0100

    Test Log
    Add TestlLog

--- src/LogTest.java
+++ src/LogTest.java
@@ -1,4 +1,11 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class LogTest {
 
+    @Test
+    void sendLog(){
+        Log log = new Log;
+        assertEquals("Works",log.writeLog("Test"));
+    }
 }

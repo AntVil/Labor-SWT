5ebf66956824e937da32d04dcc907cc13ec904fb
RafTob <83407665+RafTob@users.noreply.github.com>
2023-01-16 10:03:57 +0100

    PressureValve Test

--- src/PressureValveTest.java
+++ src/PressureValveTest.java
@@ -1,4 +1,33 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class PressureValveTest {
+
+    PressureValve valve = new PressureValve();
+
+    @BeforeEach
+    void start(){
+        valve.open();
+    }
+
+    @AfterEach
+    void end(){
+        valve.close();
+    }
+
+    @Test
+    void openValveTest(){
+        assertEquals(valve.isOpen(),true);
+        assertEquals(valve.readStatus(),1.0f);
+    }
+
+    @Test
+    void closeValveTest(){
+        valve.close();
+        assertEquals(valve.isOpen(),false);
+        assertEquals(valve.readStatus(),0.0f);
+    }
+
 
 }
